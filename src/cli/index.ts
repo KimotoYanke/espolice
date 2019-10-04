04bcfecd1245e790bb4bdc0011c2b26d703e3fe0
KimotoYanke <kimotoyanke@gmail.com>
2019-10-04 11:17:56 +0900

    event log color

--- src/cli/index.ts
+++ src/cli/index.ts
@@ -1,4 +1,10 @@
 import chalk from "chalk";
+import { alea } from "seedrandom";
+
 export const eventLog = (eventType: string, pathFromRoot: string) => {
-  console.log(chalk.white.bgCyan(eventType) + "\t" + pathFromRoot);
+  const rng = alea(eventType, { entropy: true });
+  const h = Math.floor(rng() * 360);
+  const s = Math.floor(rng() * 20) + 80;
+  const l = Math.floor(rng() * 20) + 30;
+  console.log(chalk.white.bgHsl(h, s, l)(eventType) + "\t" + pathFromRoot);
 };
